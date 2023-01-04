f961d827289175965254e699916bc95a304da5ad
Guillermo Rodrigo Palacios Soriano <guillerps1900@gmail.com>
2023-01-04 13:46:20 -0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Homepage from './components/Homepage'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Homepage />
+      <Footer />
     </>
   )
 }
