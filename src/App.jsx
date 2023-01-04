ffc48e9b3cead44344cf6137c87812f4c1b0acfe
Guillermo Rodrigo Palacios Soriano <guillerps1900@gmail.com>
2023-01-04 16:21:04 -0600

    Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Header from './components/Header'
 import Homepage from './components/Homepage'
 import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Proyectos from './components/Proyectos'
 
 function App() {
 
   return (
     <>
       <Header />
-      <Homepage />
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        <Route path='/proyectos' element={<Proyectos />} />
+      </Routes>
       <Footer />
     </>
   )
