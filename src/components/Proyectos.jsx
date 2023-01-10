1a242d6bdc885b115914c4042a110804d5fbe0ec
Guillermo Rodrigo Palacios Soriano <guillerps1900@gmail.com>
2023-01-09 18:57:15 -0600

    proyectos completos

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {FaMedal, FaChild, FaBook} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import {FaMedal, FaChild, FaBook, FaBlackTie, FaChalkboardTeacher} from 'react-icons/fa'
 
 export default function Proyectos() {
   return (
@@ -15,7 +16,7 @@ export default function Proyectos() {
         <h3 className='text-center my-20 font-bold text-3xl text-royal'>NUESTRO <span className='text-center my-20 font-bold text-3xl text-mainJam'>ENFOQUE</span></h3>
         <div className='container mx-auto flex flex-col justify-around items-center md:flex-row'>
           <div className='space-y-6 max-w-sm md:max-w-none'>
-            <div className='flex justify-center space-x-5 bg-lavender items-center flex-row p-5 rounded-md'>
+            <div className='flex justify-center space-x-5 bg-slate-100 items-center flex-row p-5 rounded-md'>
               <div className='text-mainJam'>
                 <FaMedal className='text-6xl'/>
               </div>
@@ -33,7 +34,7 @@ export default function Proyectos() {
                 <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde sunt quibusdam quia suscipit. Ratione, ullam!</p>
               </div>
             </div>
-            <div className='flex justify-center space-x-5 bg-lavender items-center flex-row p-5 rounded-md'>
+            <div className='flex justify-center space-x-5 bg-slate-100 items-center flex-row p-5 rounded-md'>
               <div className='text-mainJam'>
                 <FaBook className='text-6xl'/>
               </div>
@@ -56,7 +57,63 @@ export default function Proyectos() {
       </section>
       <section id='proyectos-proyectos'>
         <div className='container mx-auto my-20'>
-        <h3 className='text-center my-20 font-bold text-3xl text-royal'>PROYECTOS <span className='text-center my-20 font-bold text-3xl text-mainJam'>A EJECUTAR</span></h3>
+          <h3 className='text-center my-20 font-bold text-3xl text-royal'>PROYECTOS <span className='text-center my-20 font-bold text-3xl text-mainJam'>A EJECUTAR</span></h3>
+        </div>
+        <div className='container mx-auto flex flex-col space-y-20 justify-center items-center'>
+          {/* First Grid Element */}
+          <div className='flex flex-col justify-center md:grid grid-cols-7 grid-rows-2 self-end'>
+            <div className='col-start-1 max-w-sm col-end-4 row-start-1 my-12 mx-auto row-end-3 z-20 bg-slate-100 p-10 order-3 md:order-none'>
+              <FaBook className='text-6xl mb-10 text-mainBlue'/>
+              <p className='mb-10'>Enseñanza del MÉTODO DE APRENDIZAJE PERCEPTIVO-DISCRIMINATIVO dirigido a niños y jóvenes con síndrome de Down como base preparatoria para un futuro y exitoso programa de lectura y escritura según el método de enseñanza de María Victoria Troncoso y María Mercedes del Cerro (España).</p>
+              <span className='text-sm block font-bold'>A Recaudar</span>
+              <span className='text-3xl font-bold text-green-500'>$25,000</span>
+            </div>
+            <div className='col-start-3 col-end-8 row start-1 row-end-2 order-1 md:order-none'>
+              <img className='' src="../../public/p1.jpg" alt="" />
+            </div>
+            <div className='col-start-4 col-end-8 row start-2 row-end-3 order-2 md:order-none md:m-5'>
+              <h3 className='font-bold text-center md:text-left mt-4 md:mt-0 text-3xl mb-4'>Aprendiendo a mi ritmo</h3>
+              <div className='text-center md:text-left'>
+                <Link className='bg-mainJam rounded-md hover:bg-purple text-white font-bold px-3 py-2' to="/contacto">Patrocinar</Link>
+              </div>
+            </div>
+          </div>
+          {/* Second Grid Element */}
+          <div className='flex flex-col justify-center md:grid grid-cols-7 grid-rows-2 self-center'>
+            <div className='col-start-1 max-w-sm col-end-4 row-start-1 my-12 mx-auto row-end-3 z-20 bg-slate-100 p-10 order-3 md:order-none'>
+              <FaBlackTie className='text-6xl mb-10 text-mainBlue'/>
+              <p className='mb-10'>Involucramiento de jóvenes del área de bachillerato en la experimentación de primera mano del trabajo realizado por profesionales provenientes de sectores relevantes del área profesional, con el objetivo que el futuro graduado pueda tomar una mejor decisión respecto a su futuro profesional.</p>
+              <span className='text-sm block font-bold'>A Recaudar</span>
+              <span className='text-3xl font-bold text-green-500'>$19,000</span>
+            </div>
+            <div className='col-start-3 col-end-8 row start-1 row-end-2 order-1 md:order-none'>
+              <img className='' src="../../public/p2.jpg" alt="" />
+            </div>
+            <div className='col-start-4 col-end-8 row start-2 row-end-3 order-2 md:order-none md:m-5'>
+              <h3 className='font-bold text-center md:text-left mt-4 md:mt-0 text-3xl mb-4'>Una ventana a mi futuro</h3>
+              <div className='text-center md:text-left'>
+                <Link className='bg-mainJam rounded-md hover:bg-purple text-white font-bold px-3 py-2' to="/contacto">Patrocinar</Link>
+              </div>
+            </div>
+          </div>
+          {/* Third Grid Element */}
+          <div className='flex flex-col justify-center md:grid grid-cols-7 grid-rows-2 self-end'>
+            <div className='col-start-1 max-w-sm col-end-4 row-start-1 my-12 mx-auto row-end-3 z-20 bg-slate-100 p-10 order-3 md:order-none'>
+              <FaChalkboardTeacher className='text-6xl mb-10 text-mainBlue'/>
+              <p className='mb-10'>Impartición de talleres didácticos dirigido a padres, madres o cuidadores pertenecientes a comunidades en contextos de riesgo, con el objetivo de brindar herramientas de educación para cambiar la crianza tradicional autoritaria por una crianza positiva.</p>
+              <span className='text-sm block font-bold'>A Recaudar</span>
+              <span className='text-3xl font-bold text-green-500'>$33,000</span>
+            </div>
+            <div className='col-start-3 col-end-8 row start-1 row-end-2 order-1 md:order-none'>
+              <img className='' src="../../public/p2.jpg" alt="" />
+            </div>
+            <div className='col-start-4 col-end-8 row start-2 row-end-3 order-2 md:order-none md:m-5'>
+              <h3 className='font-bold text-center md:text-left mt-4 md:mt-0 text-3xl mb-4'>Eduquemos en positivo</h3>
+              <div className='text-center md:text-left'>
+                <Link className='bg-mainJam rounded-md hover:bg-purple text-white font-bold px-3 py-2' to="/contacto">Patrocinar</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
