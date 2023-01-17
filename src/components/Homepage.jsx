d08bf95aa411d17e97e4099fe8d95a13b75ce128
Guillermo Rodrigo Palacios Soriano <guillerps1900@gmail.com>
2023-01-16 18:33:52 -0600

    fixing images

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -53,7 +53,7 @@ export default function Homepage() {
           {/* First Grid Container */}
           <div className='md:grid grid-cols-6 max-w-5xl self-start'>
             <div className='col-start-1 col-end-5 row-start-1 row-end-2'>
-              <img className='w-full' src="/src/assets/voluntarios.jpg" alt="" />
+              <img className='w-full' src="../src/assets/voluntarios.jpg" alt="" />
             </div>
             <div className='col-start-4 col-end-7 bg-purple row-start-1 row-end-2 md:my-8 p-5'>
               <h3 className='text-3xl text-white md:mt-20 mb-10'>Voluntariado</h3>
